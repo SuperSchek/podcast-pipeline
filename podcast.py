c862a6294a89255f516608cc50ffb7399fc40a70
SuperSchek <sanderschekman@gmail.com>
2017-07-12 11:30:27 +0200

    Moved APP_NAME into secrets file

--- podcast.py
+++ podcast.py
@@ -13,11 +13,9 @@ try:
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
     flags = None
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/drive-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Filmerds Podcast Pipeline'
+
+## File imports
+import secrets
 
 ## Boto3 imports (for S3 upload)
 import boto3
@@ -25,8 +23,11 @@ import boto3
 ## PyDub imports (for audio conversion)
 from pydub import AudioSegment
 
-## File imports
-import secrets
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = secrets.APPLICATION_NAME
 
 def get_credentials():
     """
--- secrets_example.py
+++ secrets_example.py
@@ -1,7 +1,9 @@
-# Use this file to store all your sensitive info and rename it from secrets_example.py to secrets.py. Keep in mind that in addition to this file you will also need to obtain a client_secrets.json from Google to authorize Google Drive.
+# Use this file to store all your sensitive info and rename it from secrets_example.py to secrets.py.
+# Keep in mind that in addition to this file you will also need to obtain a client_secrets.json from Google to authorize Google Drive.
 
 ## Google Drive
-DRIVE_WATCHFOLDER_ID='DriveFolderId'
+APPLICATION_NAME='yourGoogleApiAppName'
+DRIVE_WATCHFOLDER_ID='yourDriveFolderId'
 
 ### Amazon S3
 AWS_ACCESS_KEY_ID='yourAwsAccessKeyId'
