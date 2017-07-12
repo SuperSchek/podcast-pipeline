f8e7d0a439f5636ae8ccbbf9bf907f122b9ca4a7
SuperSchek <sanderschekman@gmail.com>
2017-07-12 11:19:12 +0200

    Created check_watchfolder method using Drive REST API and cleaned up imports.

--- podcast.py
+++ podcast.py
@@ -1,34 +1,38 @@
+## Drive REST API imports
 from __future__ import print_function
 import httplib2
 import os
 import io
-
+from apiclient import errors
 from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
-
 try:
     import argparse
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
     flags = None
-
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/drive-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Filmerds Podcast Pipeline'
 
+## Boto3 imports (for S3 upload)
+import boto3
+
+## PyDub imports (for audio conversion)
+from pydub import AudioSegment
+
+## File imports
+import secrets
 
 def get_credentials():
-    """Gets valid user credentials from storage.
-
+    """
+    Gets valid user credentials from storage.
     If nothing has been stored, or if the stored credentials are invalid,
     the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
     """
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
@@ -53,9 +57,8 @@ def main():
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('drive', 'v3', http=http)
-    download(service)
-    # checkFolder()
-    # downloadPodcasts()
+
+    check_watchfolder(service)
 
 
 def download(service):
@@ -73,10 +76,21 @@ def download(service):
             request = service.files().get_media(fileId=file_id)
             fh = io.FileIO(item.get('name'), 'wb')
 
-main()
+def check_watchfolder(service):
+    page_token = None
+    while True:
+        response = service.files().list(q=("'%s' in parents and trashed = false" % secrets.DRIVE_WATCHFOLDER_ID),
+                                        spaces='drive',
+                                        fields='nextPageToken, files(id, name)',
+                                        pageToken=page_token).execute()
+        for file in response.get('files', []):
+            # Process change
+            print ('Found file: %s (%s)' % (file.get('name'), file.get('id')))
+        page_token = response.get('nextPageToken', None)
+        if page_token is None:
+            break;
 
-import boto3
-import secrets
+main()
 
 s3 = boto3.client(
     's3',
@@ -87,7 +101,6 @@ s3 = boto3.client(
 # with open('pic.png', 'rb') as data:
 #     s3.upload_fileobj(data, 'filmerds-podcast-wp', 'wp-content/picture.png', ExtraArgs={'ACL': 'public-read'})
 
-from pydub import AudioSegment
 
 # podcast = AudioSegment.from_file("podcast.mp3", "mp3")
 # podcast.export("podcast_converted.mp3", format="mp3", bitrate="128k")
