741c25937f3b0361178ecd850836953bdf04e680
SuperSchek <sanderschekman@gmail.com>
2017-07-12 11:24:42 +0200

    Updated secrets_example.py and added some instructions

--- secrets_example.py
+++ secrets_example.py
@@ -1,3 +1,8 @@
+# Use this file to store all your sensitive info and rename it from secrets_example.py to secrets.py. Keep in mind that in addition to this file you will also need to obtain a client_secrets.json from Google to authorize Google Drive.
+
+## Google Drive
+DRIVE_WATCHFOLDER_ID='DriveFolderId'
+
 ### Amazon S3
 AWS_ACCESS_KEY_ID='yourAwsAccessKeyId'
 AWS_SECRET_ACCESS_KEY='yourAwsSecretAccessKey'
